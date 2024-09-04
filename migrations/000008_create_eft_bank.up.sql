2737607f3620c41bf71291f286a147fc119b53db
Roberto Fernandez Toro <robertobouses@gmail.com>
2024-09-04 18:09:49 +0200

    feature/add-bank-transactions

--- migrations/000008_create_eft_bank.up.sql
+++ migrations/000008_create_eft_bank.up.sql
@@ -1,15 +1,13 @@
 BEGIN;
 
 CREATE TABLE bank (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    account_id UUID NOT NULL,
-    transaction_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    amount DECIMAL(12, 2) NOT NULL,
-    balance DECIMAL(12, 2) NOT NULL,
-    description TEXT,
-    transaction_type VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+    transactionid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    transactiondate TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    amount INT NOT NULL,
+    balance INT NOT NULL,
+    prospect VARCAHAR(50),
+    transactiontype VARCHAR(50) NOT NULL
+    );
 
 
 COMMIT;
