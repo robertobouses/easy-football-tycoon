07177289b100929caaa8c5d804ee5bfc2a04a514
Roberto Fernandez Toro <robertobouses@gmail.com>
2024-09-06 23:00:12 +0200

    paid purchase

--- app/process_purchase.go
+++ app/process_purchase.go
@@ -2,6 +2,8 @@ package app
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -52,6 +54,23 @@ func (a *AppService) GetCurrentProspect() (*Prospect, error) {
 }
 
 func (a *AppService) AcceptPurchase(prospect *Prospect) error {
+
+	initialFee := prospect.Fee
+
+	paid, err := a.ProcessTransferFeePaid(initialFee)
+	if err != nil {
+		return ErrPaidNotFound
+	}
+
+	initalBalance, err := a.bankRepo.GetBalance()
+	if err != nil {
+		return ErrBalanceNotFound
+	}
+
+	newBalance := initalBalance - paid
+
+	a.bankRepo.PostTransactions(paid, newBalance, prospect.ProspectName, "purchase")
+
 	team := ConvertProspectToTeam(prospect)
 	a.teamRepo.PostTeam(team)
 	return nil
@@ -75,4 +94,35 @@ func ConvertProspectToTeam(prospect *Prospect) Team {
 		InjuryDays: prospect.InjuryDays,
 		Lined:      false,
 	}
+}
+
+func (a *AppService) ProcessTransferFeePaid(initialFee int) (int, error) {
+	rand.Seed(time.Now().UnixNano())
+	randomFactor := 0.8 + rand.Float64()*1.9
+	randomFee := float64(initialFee) * randomFactor
+
+	analytics, err := a.analyticsRepo.GetAnalytics()
+	if err != nil {
+		return 0, err
+	}
+
+	var finalFee float64
+	switch {
+	case analytics.Finances <= 10:
+		finalFee = randomFee * 0.5
+	case analytics.Finances <= 30:
+		finalFee = randomFee * 0.8
+	case analytics.Finances <= 50:
+		finalFee = randomFee
+	case analytics.Finances <= 70:
+		finalFee = randomFee * 1.2
+	case analytics.Finances <= 80:
+		finalFee = randomFee * 1.3
+	case analytics.Finances <= 90:
+		finalFee = randomFee * 1.4
+	default:
+		finalFee = randomFee * 1.5
+	}
+
+	return int(finalFee), nil
 }
